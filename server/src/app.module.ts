ad7c9c6e571512bccbdfa23c625803795a320b6e
Ahmad Mayallo <ahmadmayallo02@gmail.com>
2026-03-23 01:43:57 +0200

    Connecting Database and Redis and BullMQ

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,62 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { CourseModule } from './course/course.module';
+import { TeacherModule } from './teacher/teacher.module';
+import { PlaylistModule } from './playlist/playlist.module';
+import { VideoModule } from './video/video.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bullmq';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-yet';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    CategoryModule,
+    CourseModule,
+    TeacherModule,
+    PlaylistModule,
+    VideoModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST!,
+        port: +process.env.REDIS_PORT!,
+      },
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST!,
+      port: +process.env.REDIS_PORT!,
+      ttl: 1000 * 60 * 60 * 24,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
